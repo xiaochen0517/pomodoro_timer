8e5be3d69f808e4c692588c8e111265b502d694f
mochenya <2827075398@qq.com>
2025-01-17 11:36:03 +0800

    update package config

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
-    implementation("org.jetbrains.compose.material3:material3:1.6.11")
+    implementation(compose.material3)
     implementation("androidx.compose.material:material-icons-extended:1.6.8")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
