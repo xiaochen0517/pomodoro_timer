b698b5eaa86f452adbb0fa970a7a45bf41800211
mochenya <2827075398@qq.com>
2025-01-17 11:58:32 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Exe, TargetFormat.Deb)
             packageName = "番茄时钟（Pomodoro Timer）"
-            packageVersion = "1.1.3"
+            packageVersion = "1.1.4"
 
             windows {
                 shortcut = true
